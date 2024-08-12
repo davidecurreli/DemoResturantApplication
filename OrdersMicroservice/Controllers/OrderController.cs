68dbd8d3bad1c67cdfbce37cc449764527455ea1
davidecurreli <davide.curreli95gmail.com>
2024-08-12 18:19:52 +0200

    added GetMyOrders method to OrderController

--- OrdersMicroservice/Controllers/OrderController.cs
+++ OrdersMicroservice/Controllers/OrderController.cs
@@ -7,6 +7,21 @@ namespace OrdersMicroservice.Controllers;
 
 [ApiController]
 [Route("orders/api/[controller]")]
-public class OrderController(IGenericCoreService<Order> dataService) : GenericController<Order>(dataService)
+public class OrderController(IGenericCoreService<Order> dataService, IGenericCoreService<Customer> customerService) : GenericController<Order>(dataService)
 {
+    [HttpGet]
+    [Route("GetMyOrders/{mail}")]
+    public ActionResult<dynamic> GetMyOrders(string mail)
+    {
+        var customer = customerService.GetQueryable(x=> x.Email == mail).FirstOrDefault();
+
+        if (customer is null)
+            return new GetMyOrdersResponse([]);
+
+        var orders = _dataService.GetQueryable(x => x.CustomerID == customer.Id).ToList();
+
+        return Ok(new GetMyOrdersResponse(orders));
+    }
+
+    public record GetMyOrdersResponse(List<Order>? Items);
 }
